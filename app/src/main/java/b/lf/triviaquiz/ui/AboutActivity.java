7373c55f5c0dc117a61315f352f8fd420575f15d
blf <bolf@li.ru>
2018-08-19 20:14:24 +0300

    about activity

--- app/src/main/java/b/lf/triviaquiz/ui/AboutActivity.java
+++ app/src/main/java/b/lf/triviaquiz/ui/AboutActivity.java
@@ -1,7 +1,9 @@
 package b.lf.triviaquiz.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
 
 import b.lf.triviaquiz.R;
 
@@ -11,5 +13,15 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(view -> {
+            Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+            sharingIntent.setType("text/plain");
+            String shareBody = "I use TriviaQuiz App =)!";
+            sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Did you know?");
+            sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+            startActivity(Intent.createChooser(sharingIntent, "Share via"));
+        });
     }
 }
