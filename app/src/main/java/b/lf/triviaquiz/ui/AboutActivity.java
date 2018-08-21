d7575d12a46c66a0da4b23d64ce845b80dc3c5cc
blf <bolf@li.ru>
2018-08-21 09:10:53 +0300

    ad banner added

--- app/src/main/java/b/lf/triviaquiz/ui/AboutActivity.java
+++ app/src/main/java/b/lf/triviaquiz/ui/AboutActivity.java
@@ -5,14 +5,24 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
 import b.lf.triviaquiz.R;
 
 public class AboutActivity extends AppCompatActivity {
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");//test ID
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
 
         FloatingActionButton fab = findViewById(R.id.floatingActionButton);
         fab.setOnClickListener(view -> {
