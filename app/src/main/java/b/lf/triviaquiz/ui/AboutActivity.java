951b1b2fed13dc652e319af4d67e66a3de2e64c8
blf <bolf@li.ru>
2018-08-21 23:18:06 +0300

    ad banner completed

--- app/src/main/java/b/lf/triviaquiz/ui/AboutActivity.java
+++ app/src/main/java/b/lf/triviaquiz/ui/AboutActivity.java
@@ -7,7 +7,6 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.MobileAds;
 
 import b.lf.triviaquiz.R;
 
@@ -19,9 +18,11 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");//test ID
         mAdView = findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder().build();
+
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
         mAdView.loadAd(adRequest);
 
         FloatingActionButton fab = findViewById(R.id.floatingActionButton);
